e13c2d7565a6a57c4a85825eee5e20f4d46f6d26
Anjali Sharma <57942929+aanjali12@users.noreply.github.com>
2021-03-09 20:58:57 +0530

    Add files via upload

---
+++ assignment.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar  9 20:53:53 2021
+
+@author: ANJALI
+"""
+print("question 1")
+x=int(input("Enter the value of x:"))
+y=int(input("Enter the value of y:"))
+z=pow(x,y)
+print(z)
+
+
+
+
+print("question 2")
+
+li=[12,43,6,8,567,452,13,98]
+for i in li:
+    if i%3==0:
+        print(i)
